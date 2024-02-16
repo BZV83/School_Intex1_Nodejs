22051121bd62889c8106a70928402c249c59a6ed
BZV83 <120076188+BZV83@users.noreply.github.com>
2024-02-16 12:33:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const knex = require("knex")({
   connection: {
     host: "localhost",
     user: process.env.RDS_USERNAME || "postgres",
-    password: process.env.RDS_PASSWORD || "Id0n'ttakeLs",
-    database: process.env.RDS_DB_NAME || "SocialMediaHealth",
+    password: process.env.RDS_PASSWORD || "**",
+    database: process.env.RDS_DB_NAME || "**",
     port: process.env.RDS_PORT || 5432,
     ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
   }

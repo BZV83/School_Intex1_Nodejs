4f26068c7a628356318df5aa6722957ca96dca11
BZV83 <120076188+BZV83@users.noreply.github.com>
2024-08-31 18:45:01 -0600

    Update 00_get_certificate.sh

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d smumh-group1-3.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email bzv27@byu.edu
+sudo certbot -n -d <url> --nginx --agree-tos --email <email>
